5fcc30add0eaa8d1f113e8ec02fc7564195afbcf
pyke <pykelysia0206@163.com>
2025-12-16 15:09:41 +0800

    feat(routebind): 添加路由绑定初始化

--- knowtime.go
+++ knowtime.go
@@ -3,9 +3,15 @@ package main
 import (
 	"knowtime/config"
 	"knowtime/database"
+	"knowtime/route"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	config.LoadEnv("./.env")
 	database.InitDatabase()
+	server := gin.Default()
+	route.Bind(server)
+	server.Run(":8080")
 }
---
+++ route/route.go
@@ -0,0 +1,7 @@
+package route
+
+import "github.com/gin-gonic/gin"
+
+func Bind(server *gin.Engine) (err error) {
+	return
+}
