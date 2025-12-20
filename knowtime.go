926beb6f6cc516c8a6de6f919af289d5939a4c65
pyke <pykelysia0206@163.com>
2025-12-20 16:55:24 +0800

    feat(main): 完成主程序代码

--- knowtime.go
+++ knowtime.go
@@ -6,12 +6,22 @@ import (
 	"knowtime/route"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pykelysia/pyketools"
 )
 
 func main() {
+
+	// 加载环境
 	config.LoadEnv("./.env")
+
+	// 初始化数据库
 	database.InitDatabase()
+
+	// 开启网络服务
 	server := gin.Default()
-	route.Bind(server)
-	server.Run(":8080")
+	route.Bind(server)         // 绑定路由
+	err := server.Run(":8080") // 开始监听
+	if err != nil {
+		pyketools.Fatalf("net start fail: %v", err)
+	}
 }
