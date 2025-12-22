94d950430f1aa0ea0498cfe03befb92d96fe8858
pyke <pykelysia0206@163.com>
2025-12-22 13:43:02 +0800

    comment(database/type): 添加注释

--- database/type.go
+++ database/type.go
@@ -9,10 +9,12 @@ type (
 		TimeEvents []TimeEvent `gorm:"foreignKey:UIdRefer"`
 	}
 	TimeEvent struct {
-		TimeEventId uint   `gorm:"primaryKey; autoIncrement; column:timeEventId"`
-		Date        string `gorm:"column:date"`
-		AppName     string `gorm:"column:appName"`
-		Duration    int    `gorm:"column:duration"`
-		UIdRefer    uint   `gorm:"column:uIdRefer"`
+		TimeEventId uint `gorm:"primaryKey; autoIncrement; column:timeEventId"`
+		// format yyyy-mm-dd
+		Date    string `gorm:"column:date"`
+		AppName string `gorm:"column:appName"`
+		// ms
+		Duration int  `gorm:"column:duration"`
+		UIdRefer uint `gorm:"column:uIdRefer"`
 	}
 )
