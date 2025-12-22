d00f070a7a150b54dcea55115a26d1d5a9b7715b
pyke <pykelysia0206@163.com>
2025-12-22 13:43:50 +0800

    type(internal): 定义 Internal 的类型

--- internal/type.go
+++ internal/type.go
@@ -5,4 +5,16 @@ type (
 		Code    int    `json:"code"`
 		Message string `json:"message"`
 	}
+
+	InternalUsualMsgPostReq struct {
+		AppName  string `json:"app_name"`
+		Duration int32  `json:"duration"`
+	}
+	InternalGenerateReq struct {
+		// format yyyy-mm-dd
+		Date string `json:"data"`
+	}
+	InternalGenerateResp struct {
+		Output string `json:"output"`
+	}
 )
