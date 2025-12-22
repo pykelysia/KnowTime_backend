89f2332da00e2dc859307c6a02eeebcef7d94cf8
pyke <pykelysia0206@163.com>
2025-12-22 14:37:19 +0800

    init(internal): 初始化 internal 层的 internal 函数

---
+++ internal/internal.go
@@ -0,0 +1,11 @@
+package internal
+
+func InternalUsualMsgPostInternal(i InternalUsualMsgPostReq) (BaseMsg, error) {
+
+	return BaseMsg{}, nil
+}
+
+func InternalGenerateInternal(i InternalGenerateReq) (InternalGenerateResp, BaseMsg, error) {
+
+	return InternalGenerateResp{}, BaseMsg{}, nil
+}
