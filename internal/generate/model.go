c3b1e111fc3d3becbbed6c8cfa0f4cfeae5e0fbd
pyke <pykelysia0206@163.com>
2025-12-23 16:35:44 +0800

    update(model): 添加 Model 配置

--- internal/generate/model.go
+++ internal/generate/model.go
@@ -2,14 +2,18 @@ package generate
 
 import (
 	"context"
+	"os"
 
 	"github.com/cloudwego/eino-ext/components/model/openai"
 	"github.com/cloudwego/eino/components/model"
 )
 
 func newChatModel(ctx context.Context) (cm model.ToolCallingChatModel, err error) {
-	// TODO Modify component configuration here.
-	config := &openai.ChatModelConfig{}
+	config := &openai.ChatModelConfig{
+		Model:   os.Getenv("CHAT_MODEL_NAME"),
+		BaseURL: os.Getenv("BASE_URL"),
+		APIKey:  os.Getenv("API_KEY"),
+	}
 	cm, err = openai.NewChatModel(ctx, config)
 	if err != nil {
 		return nil, err
