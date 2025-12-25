75ee46e2e818181a7f21f454d867fb27ed66ee08
pyke <pykelysia0206@163.com>
2025-12-25 09:51:13 +0800

    update(lambda) 更新 messageToString 函数内容

--- internal/generate/lambda_func.go
+++ internal/generate/lambda_func.go
@@ -8,5 +8,5 @@ import (
 
 // newLambda1 component initialization function of node 'MessageToString' in graph 'softNew'
 func messageToString(ctx context.Context, input *schema.Message) (output string, err error) {
-	panic("implement me")
+	return input.Content, nil
 }
