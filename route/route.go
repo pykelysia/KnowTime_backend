21b2b1d69753d8aba9cc62bccd834c891f38e6d4
pyke <pykehuo@gmail.com>
2026-01-11 22:49:21 +0800

    feat(route-v1): 添加 v1 路由定义，并添加处理文件

--- route/route.go
+++ route/route.go
@@ -20,5 +20,21 @@ func Bind(server *gin.Engine) (err error) {
 		internalGroup.POST("/usual-msg-post", internalUsualMsgPostHandler())
 		internalGroup.POST("/generate", internalGenerateHandler())
 	}
+
+	routeV1 := server.Group("/v1")
+	{
+		msgGroup := routeV1.Group("/msg")
+		{
+			msgGroup.Use(middleware.JWTAuthMiddleware())
+
+			msgGroup.POST("/post")
+		}
+		reportGroup := routeV1.Group("/report")
+		{
+			reportGroup.Use(middleware.JWTAuthMiddleware())
+			reportGroup.GET("/:date")
+		}
+	}
+
 	return
 }
