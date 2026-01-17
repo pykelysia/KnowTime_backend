2766052a29b5a898483c80b1f27f8f9d8e615cc0
lvshujun <Lvshujun0918@163.com>
2026-01-17 14:15:42 +0800

    feat(route-v1): 修改API适配

--- route/route.go
+++ route/route.go
@@ -2,6 +2,7 @@ package route
 
 import (
 	"knowtime/middleware"
+	"knowtime/route/v1/handler"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,26 +15,19 @@ func Bind(server *gin.Engine) (err error) {
 		userGroup.GET("/info/:u_id", middleware.JWTAuthMiddleware(), userInfo())
 	}
 
-	internalGroup := server.Group("/internal")
-	internalGroup.Use(middleware.JWTAuthMiddleware())
-	{
-		internalGroup.POST("/usual-msg-post", internalUsualMsgPostHandler())
-		internalGroup.POST("/generate", internalGenerateHandler())
-	}
-
 	routeV1 := server.Group("/v1")
 	{
 		msgGroup := routeV1.Group("/msg")
 		{
 			msgGroup.Use(middleware.JWTAuthMiddleware())
 
-			msgGroup.POST("/post")
+			msgGroup.POST("/post", handler.MsgPostHandler())
 		}
 		reportGroup := routeV1.Group("/report")
 		{
 			reportGroup.Use(middleware.JWTAuthMiddleware())
 
-			reportGroup.GET("/:date")
+			reportGroup.GET("/:date", handler.GenerateHandler())
 		}
 	}
 
--- route/v1/handler/msg.go
+++ route/v1/handler/msg.go
@@ -1,1 +1,49 @@
 package handler
+
+import (
+	"knowtime/internal"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func MsgPostHandler() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		userIDFromJWT, exists := ctx.Get("user_id")
+		userIDFromParma, err := strconv.Atoi(ctx.Query("u_id"))
+		if !exists || userIDFromJWT.(uint) != uint(userIDFromParma) || err != nil {
+			ctx.JSON(http.StatusUnauthorized, BaseMsg{
+				Code:    401,
+				Message: "User not authenticated",
+			})
+			return
+		}
+
+		var i internal.InternalUsualMsgPostReq
+		if err := ctx.ShouldBindJSON(&i); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"net_message": BaseMsg{
+					Code:    400,
+					Message: "Invalid request body",
+				},
+			})
+			return
+		}
+
+		b, err := internal.InternalUsualMsgPostInternal(userIDFromJWT.(uint), i)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"net_message": b,
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"net_message": b,
+			"data": map[string]any{
+				"success": true,
+			},
+		})
+	}
+}
