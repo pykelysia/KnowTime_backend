9b1fd50c404d28549dea39831daa43458d2b2f70
lvshujun <Lvshujun0918@163.com>
2026-01-21 11:34:29 +0800

    fix(model): 默认采用已获得的配置中的模型参数

--- internal/chat/model.go
+++ internal/chat/model.go
@@ -2,19 +2,17 @@ package chat
 
 import (
 	"context"
-	"os"
+	"knowtime/config"
 
 	"github.com/cloudwego/eino-ext/components/model/openai"
 	"github.com/cloudwego/eino/components/model"
 )
 
-// newChatModel component initialization function of node 'Chat' in graph 'chat'
 func newChatModel(ctx context.Context) (cm model.ToolCallingChatModel, err error) {
-	// TODO Modify component configuration here.
 	config := &openai.ChatModelConfig{
-		Model:   os.Getenv("CHAT_MODEL_NAME"),
-		BaseURL: os.Getenv("BASE_URL"),
-		APIKey:  os.Getenv("API_KEY"),
+		Model:   config.ChatModelName,
+		BaseURL: config.BaseURL,
+		APIKey:  config.APIKey,
 	}
 	cm, err = openai.NewChatModel(ctx, config)
 	if err != nil {
--- internal/generate/model.go
+++ internal/generate/model.go
@@ -2,7 +2,7 @@ package generate
 
 import (
 	"context"
-	"os"
+	"knowtime/config"
 
 	"github.com/cloudwego/eino-ext/components/model/openai"
 	"github.com/cloudwego/eino/components/model"
@@ -10,9 +10,9 @@ import (
 
 func newChatModel(ctx context.Context) (cm model.ToolCallingChatModel, err error) {
 	config := &openai.ChatModelConfig{
-		Model:   os.Getenv("CHAT_MODEL_NAME"),
-		BaseURL: os.Getenv("BASE_URL"),
-		APIKey:  os.Getenv("API_KEY"),
+		Model:   config.ChatModelName,
+		BaseURL: config.BaseURL,
+		APIKey:  config.APIKey,
 	}
 	cm, err = openai.NewChatModel(ctx, config)
 	if err != nil {
